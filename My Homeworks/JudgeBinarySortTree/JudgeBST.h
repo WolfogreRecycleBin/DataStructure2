edda964b74b86a28c0b2dfd93d02111551668b5e
wolfogre <wolfogre@qq.com>
2015-04-28 21:08:26 +0800

    JudgeBinarySortTree: added it and finished it new file:   My Homeworks/JudgeBinarySortTree/Assistance.h new file:   My Homeworks/JudgeBinarySortTree/BinTreeNode.h new file:   My Homeworks/JudgeBinarySortTree/BinaryTree.h new file:   My Homeworks/JudgeBinarySortTree/JudgeBST.h new file:   My Homeworks/JudgeBinarySortTree/JudgeBinarySortTree.mdsp new file:   My Homeworks/JudgeBinarySortTree/LinkQueue.h new file:   My Homeworks/JudgeBinarySortTree/Node.h new file:   My Homeworks/JudgeBinarySortTree/TestJudgeBST.cpp

---
+++ My Homeworks/JudgeBinarySortTree/JudgeBST.h
@@ -0,0 +1,63 @@
+#ifndef __JUDGE_BST_H__
+#define __JUDGE_BST_H__
+
+#include "BinaryTree.h"
+#include "BinTreeNode.h"
+
+#include <iostream>
+using namespace std;
+
+template <class ElemType>
+bool JudgeBST(const BinaryTree<ElemType> &tree);
+
+template <class ElemType>
+bool CheckSubtree(BinTreeNode<ElemType> *sub_root, ElemType *min, ElemType *max);
+
+template <class ElemType>
+bool JudgeBST(const BinaryTree<ElemType> &tree)
+{
+	BinTreeNode<ElemType> *root = tree.GetRoot();
+	ElemType *l_min = NULL;
+	ElemType *l_max = new ElemType(root->data);
+	ElemType *r_min = new ElemType(root->data);
+	ElemType *r_max = NULL;
+	
+	bool result = true;
+	if(!CheckSubtree(root->leftChild,l_min,l_max) || !CheckSubtree(root->rightChild,r_min,r_max))
+		result = false;
+	if(l_max != NULL)	delete l_max;
+	if(l_min != NULL)	delete l_min;
+	if(r_max != NULL)	delete r_max;
+	if(r_min != NULL)	delete r_min;
+	return result;
+}
+
+template <class ElemType>
+bool CheckSubtree(BinTreeNode<ElemType> *sub_root, ElemType *min, ElemType *max)
+{
+	if(sub_root == NULL)
+		return true;
+	if(max != NULL && sub_root->data > *max || min != NULL && sub_root->data < *min)
+		return false;
+
+	ElemType *l_max = new ElemType(sub_root->data);
+	ElemType *l_min = NULL;
+	ElemType *r_max = NULL;
+	ElemType *r_min = new ElemType(sub_root->data);
+
+	if(min != NULL)
+		l_min = new ElemType(*min);
+	if(max != NULL)
+		r_max = new ElemType(*max);
+
+	bool result = true;
+	if(!CheckSubtree(sub_root->leftChild,l_min,l_max) || !CheckSubtree(sub_root->rightChild,r_min,r_max))
+		result = false;
+	if(l_max != NULL)	delete l_max;
+	if(l_min != NULL)	delete l_min;
+	if(r_max != NULL)	delete r_max;
+	if(r_min != NULL)	delete r_min;
+	return result;
+}
+
+#endif
